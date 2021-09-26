fd6b82cc2966cad6db12fa6bf299b9698810e04a
Bhargav <bhargavkadali39@gmail.com>
2021-09-26 22:40:24 +0530

    Update myQR.py

--- myQR.py
+++ myQR.py
@@ -1,1 +1,19 @@
 
+import qrcode
+import numpy as np
+
+data = input('Enter data for the barcode: ')
+
+qr = qrcode.QRCode(version=1, box_size=10, border=4)
+
+qr.add_data(data)
+
+qr.make()
+
+print("Dimensions of the QR code:", np.array(qr.get_matrix()).shape)
+
+img = qr.make_image(fill_color="white", back_color="black")
+
+img.save("site_inversed.png")
+
+img.show("site_inversed.png")
